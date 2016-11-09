51045d5f807526cf6d101f606816bab870535d57
KL Tah <kltah1@gmail.com>
2016-11-09 04:28:19 +0000

    213 loops
    use forEach in template on passed in array of objects to print posts

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -7,12 +7,24 @@ app.get('/', function(req,res) {
     res.render("home.ejs");
 })
 
+app.get('/posts', function(req, res) {
+    var posts = [
+            {title:"post 1", author: "Susy"},
+            {title:"post 2", author: "Tom"},
+            {title:"post 3", author: "Dick"}
+        ];
+    res.render("posts.ejs", {posts: posts})
+})
 
 app.get('/fallinlovewith/:thing', function(req,res) {
     var thing = req.params.thing;
     res.render("love.ejs",{thing: thing});
 })
 
+
+app.get('*', function(req, res) {
+    res.render("404.ejs");
+})
 
 
 
