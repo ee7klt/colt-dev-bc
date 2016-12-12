0ce3b68079e4062cbca8cf06107bd8f775a3d534
KL Tah <kltah1@gmail.com>
2016-12-12 23:45:43 +0800

    225. movie search using GET method instaed of POST method
    GET less secure than POST because form data is visible in url

--- API/movie_search_app/app.js
+++ API/movie_search_app/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-let endpoint = 'http://www.omdbapi.com/?s='
+
 
 app.get('/', function(req,res) {
     res.send("home");
@@ -21,18 +21,21 @@ app.get('/', function(req,res) {
 app.get('/search',function(req,res) {
 res.render('search')
 console.log('search page')
+  //endpoint=endpoint+req.query.search
   //res.redirect('/results');
 })
 
-app.post('/requestKeyword', function(req,res) {
-  console.log('requestKeyword POST route reached')
-  console.log(req.body.keyword)
-  endpoint=endpoint+req.body.keyword
-  res.redirect('/results')
-})
+// app.post('/requestKeyword', function(req,res) {
+//   console.log('requestKeyword POST route reached')
+//   console.log(req.body.keyword)
+//   endpoint=endpoint+req.body.keyword
+//   res.redirect('/results')
+// })
 
 
 app.get('/results', function(req,res) {
+  let endpoint = 'http://www.omdbapi.com/?s='
+  endpoint=endpoint+req.query.search
   request(endpoint, function(error, response, body) {
     if(!error && response.statusCode == 200) {
     const data = JSON.parse(body)
