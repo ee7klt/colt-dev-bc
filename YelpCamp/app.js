3060512fb0ebd5edc970ffe9c9c66c608b3975df
ee7klt <kltah1@gmail.com>
2017-01-01 15:02:33 +0800

    228. text overlay on images

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,7 +4,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-const campgrounds = ['Ootoro Creek', 'Hamachi Mountain', 'Uni River'];
+const campgrounds = [
+  {name: 'Ootoro Creek',  image:"http://lorempixel.com/400/200/"},
+  {name: 'Hamachi Mountain',  image:"http://lorempixel.com/400/200/"},
+  {name: 'Uni River', image:"http://lorempixel.com/400/200/"}];
 
 app.get('/', function(req,res) {
     res.render("landing");
---
+++ YelpCamp/public/script.js
@@ -0,0 +1,5 @@
+const campgroundName = getElementsByClassName('campgroundName')[0];
+
+window.onload = function() {
+  campgroundName.classList.remove('preload')
+}
