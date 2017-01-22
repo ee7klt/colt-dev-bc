e1ca0ce13186e575409d0148d861a76e7a808579
KL Tah <kltah1@gmail.com>
2017-01-22 11:29:30 +0800

    243 adding mongoose to yelpcamp. my solution

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,12 +5,36 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/campsDB');
+var campSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+var camp = mongoose.model('camp', campSchema);
+
+
 
 
 let campgrounds = [
   {name: 'Ootoro Creek',  image:"http://lorempixel.com/400/200/"},
   {name: 'Hamachi Mountain',  image:"http://lorempixel.com/400/200/"},
   {name: 'Uni River', image:"http://lorempixel.com/400/200/"}];
+
+campgrounds.forEach(x =>
+  camp.find({name: x.name}, function(absent, present) {
+    if (absent) {
+      camp.create(x,
+      function(err,res) {
+        if (err) {console.log('unable to insert object in to db') }
+        else {console.log('successfully inserted' + res)}
+      })
+    }
+  })
+
+
+
+
 
 app.get('/', function(req,res) {
     res.render("landing");
