42d922ace605b3dbe18cd5034e62fb783a6626ba
KL Tah <kltah1@gmail.com>
2017-01-22 10:32:22 +0800

    mongoose pt 2: add object using MODEL.create(OBJ,CB)

--- databases/cats.js
+++ databases/cats.js
@@ -10,19 +10,39 @@ var catSchema = new mongoose.Schema({
 var catModel = mongoose.model('cat', catSchema)
 
 // add cat to db
-var george = new catModel({
-  name: "George",
-  age: 11,
-  temperament: "Grouchy"
-})
+// var george = new catModel({
+//   name: "Mrs. Norris",
+//   age: 1,
+//   temperament: "Evil"
+// })
 
-george.save(function(err, res) {
+// george.save(function(err, res) {
+//   if (err) {
+//     console.log("cannot save")
+//   } else {
+//     console.log("saved!")
+//     console.log(res)
+//
+//   }
+// });
+
+catModel.create({
+  name: "one less",
+  age: 14
+}, function(err, res) {
   if (err) {
-    console.log("cannot save")
+    console.log("oh no!")
   } else {
-    console.log("saved!")
     console.log(res)
   }
-});
+})
 
 // retrieve all cats from db
+catModel.find({}, function(err, res) {
+  if(err) {
+    console.log('oh no!')
+  } else {
+    console.log('all the cats')
+    console.log(res)
+  }
+})
