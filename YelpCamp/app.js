291b5bfcd86105bc758038e37ff35a29cb626765
KL Tah <kltah1@gmail.com>
2017-01-24 13:37:00 +0800

    244. show page - my solution

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/campsDB');
+
+// camp Schema
 var campSchema = new mongoose.Schema({
   name: String,
   image: String
@@ -17,9 +19,9 @@ var camp = mongoose.model('camp', campSchema);
 
 
 let campgrounds = [
-  {name: 'Ootoro Creek',  image:"http://lorempixel.com/400/200/"},
-  {name: 'Hamachi Mountain',  image:"http://lorempixel.com/400/200/"},
-  {name: 'Uni River', image:"http://lorempixel.com/400/200/"}
+  {name: 'Ootoro Creek',  image:"http://lorempixel.com/100/100/"},
+  {name: 'Hamachi Mountain',  image:"http://lorempixel.com/200/100/"},
+  {name: 'Uni River', image:"http://lorempixel.com/200/100/"}
 ];
 
 campgrounds.forEach(x => {
@@ -43,18 +45,31 @@ app.get('/', function(req,res) {
     res.render("landing");
 })
 
+
+//INDEX: show all campgrounds + NEW: form to submit campground
 app.get('/campgrounds', function(req,res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    camp.find({}, (err, campgrounds) => {
+      if (err) {
+        console.log('unable to retrieve campgrounds')
+      } else {
+        console.log('retrieved campgrounds')
+        //console.log(campgrounds)
+        res.render("campgrounds", {campgrounds: campgrounds})
+      }
+    })
+
 })
 
+
+// CREATE: add new campground to database
 app.post('/campgrounds', function(req,res) {
   //campgrounds.push({name:req.body.camp, image:"http://lorempixel.com/400/200/"})
- camp.create({name:req.body.camp, image:"http://lorempixel.com/400/200/"}, function(err,res) {
+ camp.create({name:req.body.camp, image:"http://lorempixel.com/200/100/"}, function(err,res) {
    if(err) {
      console.log('error creating new document')
    } else {
      console.log('new document created')
-     console.log(res)
+     //console.log(res)
    }
  })
   // redirect defaults as GET.
@@ -63,6 +78,13 @@ app.post('/campgrounds', function(req,res) {
   res.redirect('/campgrounds')
 })
 
+
+// SHOW
+app.get('/campgrounds/:id', function(req, res) {
+  var campground = req.params.id;
+  console.log('found route for ' + campground)
+  res.render('thisCamp', {campground:campground});
+})
 
 app.listen(3000, function() {
     console.log('server started!');
