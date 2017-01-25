cd1a2550308b3db688b623ca17a2ca5460471cb8
KL Tah <kltah1@gmail.com>
2017-01-25 12:21:06 +0800

    245 Query by ObjectId
    Use require('mongoose').Types.ObjectId

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -11,52 +11,73 @@ mongoose.connect('mongodb://localhost/campsDB');
 // camp Schema
 var campSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 })
+
 var camp = mongoose.model('camp', campSchema);
 
 
 
 
 let campgrounds = [
-  {name: 'Ootoro Creek',  image:"http://lorempixel.com/100/100/"},
-  {name: 'Hamachi Mountain',  image:"http://lorempixel.com/200/100/"},
-  {name: 'Uni River', image:"http://lorempixel.com/200/100/"}
+  {name: 'Ootoro Creek',  image:"http://lorempixel.com/100/100/", description:"Smooth and fatty"},
+  {name: 'Hamachi Mountain',  image:"http://lorempixel.com/200/100/", description: "bouncy"},
+  {name: 'Uni River', image:"http://lorempixel.com/200/100/", description:"tangy"}
 ];
 
+// camp.create(campgrounds[0], (err,res) => {
+//   if (err) {
+//     console.log('failure')
+//   }
+//   else {
+//     console.log('success')
+//   }
+// })
+
 campgrounds.forEach(x => {
-  camp.find({name: x.name}, function(absent, present) {
-    if (absent) {
-      camp.create(x,
-      function(err,res) {
-        if (err) {console.log('unable to insert object in to db') }
-        else {console.log('successfully inserted' + res)}
-      })
-    }
-  })
-}
-  )
+  //console.log('looping over campgrounds')
+  camp.find({name: x.name}, function(err, res) {
+    if (err) {
+      console.log('error querying collection')
+    } else {
+      if (res.length === 0) {
+        console.log('campground '+x.name+' absent. creating ...')
+        camp.create(x,
+          function(err,res) {
+            if (err) {console.log('unable to insert object in to db') }
+            else {console.log('successfully inserted' + res)}
+          })
+        }
+      else {
+        console.log('campground '+x.name+' already present. skipping')
+        console.log(res.length)
+      }
+      }
+    })
+  }
+)
 
 
 
 
 
 app.get('/', function(req,res) {
-    res.render("landing");
+  res.render("landing");
 })
 
 
 //INDEX: show all campgrounds + NEW: form to submit campground
 app.get('/campgrounds', function(req,res) {
-    camp.find({}, (err, campgrounds) => {
-      if (err) {
-        console.log('unable to retrieve campgrounds')
-      } else {
-        console.log('retrieved campgrounds')
-        //console.log(campgrounds)
-        res.render("campgrounds", {campgrounds: campgrounds})
-      }
-    })
+  camp.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log('unable to retrieve campgrounds')
+    } else {
+      console.log('retrieved campgrounds')
+      //console.log(campgrounds)
+      res.render("campgrounds", {campgrounds: campgrounds})
+    }
+  })
 
 })
 
@@ -64,14 +85,14 @@ app.get('/campgrounds', function(req,res) {
 // CREATE: add new campground to database
 app.post('/campgrounds', function(req,res) {
   //campgrounds.push({name:req.body.camp, image:"http://lorempixel.com/400/200/"})
- camp.create({name:req.body.camp, image:"http://lorempixel.com/200/100/"}, function(err,res) {
-   if(err) {
-     console.log('error creating new document')
-   } else {
-     console.log('new document created')
-     //console.log(res)
-   }
- })
+  camp.create({name:req.body.camp, image:"http://lorempixel.com/200/100/", description:"description here"}, function(err,res) {
+    if(err) {
+      console.log('error creating new document')
+    } else {
+      console.log('new document created')
+      //console.log(res)
+    }
+  })
   // redirect defaults as GET.
   // so even though we have two routes with campgrounds
   // will choose teh GET.
@@ -79,13 +100,21 @@ app.post('/campgrounds', function(req,res) {
 })
 
 
-// SHOW
+// SHOW: shows more info about 1 campground
 app.get('/campgrounds/:id', function(req, res) {
-  var campground = req.params.id;
-  console.log('found route for ' + campground)
-  res.render('thisCamp', {campground:campground});
+  var id = req.params.id;
+  camp.find({_id: mongoose.Types.ObjectId(id)}, function(err, campground) {
+    if (err) {
+      console.log(id + ' not found.')
+    } else {
+      console.log('found route for ' + id)
+      res.render('show', {campground: campground});
+    }
+  })
+
+
 })
 
 app.listen(3000, function() {
-    console.log('server started!');
+  console.log('server started!');
 })
