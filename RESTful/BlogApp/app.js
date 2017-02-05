293b462b97dac2118919db6659a10f35c7fa1aa1
KL Tah <kltah1@gmail.com>
2017-02-05 14:41:12 +0800

    247 RESTful Blog 1 - Index
    set up boiler plate for forms, database. create index page for blogs

---
+++ RESTful/BlogApp/app.js
@@ -0,0 +1,68 @@
+'use strict'
+const express=require('express')
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+
+
+
+
+// FORMS
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+// app.post(‘/ENDPOINT', function(req,res) {
+//     console.log(req.body.KEY)
+//     res.redirect(“/friends");
+// })
+
+// Database
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/BlogApp")   // will create DBNAME if not present
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: {type: String, default: "placeholder.jpg"},
+  body: String,
+  created: {type: Date, default: Date.now}
+})
+const blogModel = mongoose.model("blog", blogSchema)   // MODEL is constructor of plural version of COLL_SING
+blogModel.create(
+  {title: 'My first blog', body: 'This is my first blog'},
+  (err, res) => {
+    if (err) {
+      console.log('error inserting in to BlogApp')
+    } else console.log('success inserting in to BlogApp')
+  })  // insert OBJ
+ // find MATCH_OBJ
+
+
+
+app.get('/', function(req,res) {
+    res.send("home");
+})
+
+// RESTFUL ROUTES
+// Index
+app.get('/blogs', function(req, res) {
+  blogModel.find({}, (err,blogs) => {
+    if (err) {
+      console.log('cannot find in database')
+    } else {
+      res.render("blogs", {blogs: blogs});
+    }
+  })
+
+})
+// New
+// Create
+// Show
+// Edit
+// Update
+// Destroy
+
+
+//local
+app.listen(3000, function() {
+    console.log('server started!');
+})
