e84022b1be514c30ec787a52cf0694587a171af6
KL Tah <kltah1@gmail.com>
2017-02-17 05:32:44 +0800

    251 edit/update

--- RESTful/BlogApp/app.js
+++ RESTful/BlogApp/app.js
@@ -1,13 +1,48 @@
 'use strict'
 const express=require('express')
 const app = express();
+const marked = require('marked')
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 
 
+// MARKED
+// marked.setOptions({
+//   renderer: new marked.Renderer(),
+//   gfm: true,
+//   tables: true,
+//   breaks: false,
+//   pedantic: false,
+//   sanitize: false,
+//   smartLists: true,
+//   smartypants: false
+// });
 
+// Async highlighting with pygmentize-bundled
+marked.setOptions({
+  highlight: function (code, lang, callback) {
+    require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
+      callback(err, result.toString());
+    });
+  }
+});
 
+// Using async version of marked
+var markdownString = '```js\n console.log("hello"); \n var x = 1 \n function square(x) = x^2```';
+var msg;
+marked(markdownString, function (err, content) {
+  if (err) throw err;
+  //console.log(content);
+  msg = content;
+});
+
+//console.log(marked('I am using __markdown__.'));
+//var msg =   marked('# Marked in browser\n\n ## Rendered by *marked*. ```var x = 1;```');
+//var msg = marked('```js\n console.log("hello"); \n```')
+app.get('/marked', function(req,res) {
+    res.render("marked",{msg:msg});
+})
 
 // FORMS
 const bodyParser = require('body-parser');
@@ -26,13 +61,19 @@ const blogSchema = new mongoose.Schema({
   body: String,
   created: {type: Date, default: Date.now}
 })
+
+
 const blogModel = mongoose.model("blog", blogSchema)   // MODEL is constructor of plural version of COLL_SING
+
 blogModel.create(
   {title: 'My first blog', body: 'This is my first blog'},
   (err, res) => {
     if (err) {
       console.log('error inserting in to BlogApp')
-    } else console.log('success inserting in to BlogApp')
+    } else {
+      console.log(res)
+      console.log('success inserting in to BlogApp')
+    }
   })  // insert OBJ
  // find MATCH_OBJ
 
@@ -62,17 +103,21 @@ app.get('/blogs/new', function(req, res) {
 })
 // Create - create new post then redirect somewhere (POST)
 app.post('/blogs', function(req,res) {
-    //console.log(req.body.title)
+
+  if (req.body.blog.image == '') {
+    req.body.blog.image = 'placeholder.jpg'
+  }
+  console.log(req.body.blog)
     blogModel.create(
       req.body.blog,
       (err, blog) => {
         if (err) {
-          console.log(req.body.blog)
+          //console.log(req.body.blog)
           console.log('error insertin in to BlogApp')
           res.render('new')
         } else {
           //console.log('success creating new blog')
-          console.log(blog)
+          //console.log(blog)
           res.redirect("/");
         }
       })
@@ -89,18 +134,37 @@ app.get('/blogs/:id', function(req,res) {
   blogModel.find({"_id":id}, (err,blog) => {
     if (err) {
       console.log('cannot find in database')
+      res.redirect('/blogs')
     } else {
       console.log(blog)
       res.render("show", {blog: blog[0]});
     }
   })
 })
+
+
 // Edit
+app.get('/blogs/:id/edit', function(req, res) {
+  const id = req.params.id
+  blogModel.find({"_id":id}, (err,blog) => {
+    if (err) {
+      console.log('cannot find in database')
+      res.redirect('/blogs/'+id)
+    } else {
+      console.log(blog)
+      res.render("edit", {blog: blog[0]});
+    }
+  })
+})
+
+
+
+
 // Update
 // Destroy
 
 
 //local
-app.listen(3000, function() {
+app.listen(3030, function() {
     console.log('server started!');
 })
