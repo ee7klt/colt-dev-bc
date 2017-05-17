ca7f93faf0b2c7a814bb87170b92f48f2669b269
KL Tah <kltah1@gmail.com>
2017-05-17 13:57:50 +0800

    object references

--- associations/references.js
+++ associations/references.js
@@ -24,26 +24,47 @@ var userSchema = new mongoose.Schema({
 });
 
 var User = mongoose.model("User", userSchema);
+
+// create the user
 // User.create({
 //   email: "bob@gmail.com",
 //   name: "Bob Belcher"
 // })
 
+// create the post and associate the post with the user
 // Post.create({
-//   title: "How to cook the best burger pt 4",
+//   title: "How to cook the best burger pt 2",
 //   content: "Mustard!"
 // }, (err,post) => {
 //   console.log(post);
-//      User.findOne({name: 'Bob Belcher'}, (err,user) => {
-//        console.log('found user')
-//        user.posts.push(post)
-//        user.save((err,data) => {
-//          console.log(data)
-//        })
-//      })
+//   User.findOne({email: "bob@gmail.com"}, (err, user) => {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       user.posts.push(post);
+//       user.save((err, document) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(document);
+//         }
+//       })
+//     }
+//   })
 // })
 
+// DEPRECATED. tbh i don't know that this is doing
+// associate the post with the user
+// User.findOne({name: 'Bob Belcher'}, (err,user) => {
+//   console.log('found user')
+//   user.posts.push(post)
+//   user.save((err,data) => {
+//     console.log(data)
+//   })
+// })
 
+// this version using promises doesn't work yet
+// need to alter it to use async/await because have to access variable earlier in the chain
 // var postPromise = Post.create({
 //       title: "How to cook the best burger pt 4",
 //       content: "Mustard!"
@@ -60,9 +81,8 @@ var User = mongoose.model("User", userSchema);
 //     console.log(err)
 //   })
 
-User.findOne({name: 'Bob Belcher'}).populate("posts").exec((err,user) => {
-  if (err) console.log(err)
-  else {
-    console.log(user)
-  }
-})
+// find user then find all posts for that user
+const userPromise = User.findOne({email: 'bob@gmail.com'}).populate("posts").exec();
+userPromise.then((user) => {
+  console.log(user)
+});
