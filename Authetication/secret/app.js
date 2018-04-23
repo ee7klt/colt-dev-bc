ada0189f7df3823ad035b21a0846d8a399a5b395
KL Tah <kltah1@gmail.com>
2018-04-24 00:34:32 +0800

    secret page code along pt 2 before adding passport-local-mongoose to User schema

---
+++ Authetication/secret/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+app.set('view engine','ejs');
+app.set('port',process.env.PORT || 3400);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/auth_demo_app');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = require('./models/user');
+
+// set root route
+app.get('/', function(req,res){
+  res.render('home');
+})
+
+app.listen(app.get('port'), function(){
+  console.log('server started on port ' + app.get('port'));
+})
+
+app.get('/secret', function(req,res){
+  res.render('secret');
+})
