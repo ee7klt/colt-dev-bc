d5e8d1523d146a2eae0a4bc1f0ed3585b98ee3fc
KL Tah <kltah1@gmail.com>
2018-05-10 00:24:15 +0800

    secret pt 2 create user model and configure passport

--- Authetication/secret/app.js
+++ Authetication/secret/app.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
+app.use(require('express-session')(
+  {secret:'secretkey',
+  resave: false,
+  saveUninitialized: false}
+))
 app.set('view engine','ejs');
+
 app.set('port',process.env.PORT || 3400);
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/auth_demo_app');
 var passport = require('passport');
+
 var bodyParser = require('body-parser');
 var LocalStrategy = require('passport-local');
 var passportLocalMongoose = require('passport-local-mongoose');
 var User = require('./models/user');
-
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // set root route
 app.get('/', function(req,res){
   res.render('home');
