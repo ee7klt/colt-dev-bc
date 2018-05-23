fb0153ea62a72bb5947a8c6f4e70c0f4f261a426
HackProSU <hackprosu@Tahs-iMac.local>
2018-05-23 15:58:55 +0800

    complete follow along 3

--- Authetication/secret/app.js
+++ Authetication/secret/app.js
@@ -20,15 +20,45 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(bodyParser.urlencoded({extended:true}))
+// =================
+// ROUTES
+// =================
+
+
+
 // set root route
 app.get('/', function(req,res){
   res.render('home');
 })
+app.get('/secret', function(req,res){
+  res.render('secret');
+})
+
+// Auth Routes
+
+// show sign up form
+app.get('/register', function(req,res){
+  res.render('register')
+})
+// handling user sign up
+app.post('/register', function(req,res){
+  User.register(new User({username: req.body.username}),
+                req.body.password,
+                function(err,user){
+                  if(err) {
+                    console.log(err);
+                    return res.render('register');
+                  }
+                  passport.authenticate('local')(req, res, function(){
+                    res.redirect('/secret');
+                  })
+                }
+  )
+})
+
+
 
 app.listen(app.get('port'), function(){
   console.log('server started on port ' + app.get('port'));
-})
-
-app.get('/secret', function(req,res){
-  res.render('secret');
 })
