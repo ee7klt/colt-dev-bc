e789419d56ea79b82b815eb44555bc199d9cb388
ee7klt <kltah1@gmail.com>
2019-07-28 21:57:56 +0800

    compare campground creator id with user id. before factoring in to middleware

--- YelpCamp/v10/routes/show.js
+++ YelpCamp/v10/routes/show.js
@@ -3,19 +3,30 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
 var camp = require('../models/camp');
-
+var user = require('../models/user');
 // EDIT camp
 router.get('/campgrounds/:id/edit',function(req,res) {
   var id = req.params.id;
-  camp.findById(id, (err, campground) => {
-    if (err) {
-      console.log('unable to retrieve camground for comment')
-    } else {
-      console.log('retrieved campground for edit')
-      res.render("show/edit", {campground: campground})
-    }
-  })
+  if(req.isAuthenticated()) {
+    camp.findById(id, (err, campground) => {
+      if (err) {
+        console.log('unable to retrieve camground for comment')
+      } else {
+        console.log('retrieved campground for edit')
+        console.log(typeof campground.userid)
+        console.log(typeof req.user._id)
+        if (req.user._id.equals(campground.userid)) {
+          res.render("show/edit", {campground: campground})
+        } else {
+          res.send('you are not authorized to edit this camp either because you are not logged in or because this camp does not belong to you')
+        }
 
+      }
+    })
+  } else {
+    console.log("you need to be logged in to do that");
+    res.send("you need to be logged in to do that");
+  }
 })
 
 // DELETE camp
@@ -36,7 +47,7 @@ router.delete('/campgrounds/:id/',function(req,res) {
 router.put('/campgrounds/:id/',function(req,res) {
   var id = req.params.id;
   var campground = req.body.campground;
-console.log(req.body)
+  console.log(req.body)
   camp.findByIdAndUpdate(id, campground, (err, campground) => {
     if (err) {
       console.log('unable to retrieve camground for update')
--- YelpCamp/v10/seeds.js
+++ YelpCamp/v10/seeds.js
@@ -13,18 +13,26 @@ const seedDB = () => {
     {
       name: 'Torres del Paine',
       image:"/images/camp1.jpg",
-      description:"Torres del Paine National Park (Spanish: Parque Nacional Torres del Paine)[3] is a national park encompassing mountains, glaciers, lakes, and rivers in southern Chilean Patagonia. The Cordillera del Paine is the centerpiece of the park. It lies in a transition area between the Magellanic subpolar forests and the Patagonian Steppes. The park is located 112 km (70 mi) north of Puerto Natales and 312 km (194 mi) north of Punta Arenas. The park borders Bernardo O'Higgins National Park to the west and the Los Glaciares National Park to the north in Argentine territory. Paine means <em>blue</em> in the native Tehuelche (Aonikenk) language and is pronounced PIE-nay."
+      description:"Torres del Paine National Park (Spanish: Parque Nacional Torres del Paine)[3] is a national park encompassing mountains, glaciers, lakes, and rivers in southern Chilean Patagonia. The Cordillera del Paine is the centerpiece of the park. It lies in a transition area between the Magellanic subpolar forests and the Patagonian Steppes. The park is located 112 km (70 mi) north of Puerto Natales and 312 km (194 mi) north of Punta Arenas. The park borders Bernardo O'Higgins National Park to the west and the Los Glaciares National Park to the north in Argentine territory. Paine means <em>blue</em> in the native Tehuelche (Aonikenk) language and is pronounced PIE-nay.",
+      user: "novak",
+      userid : "5cc966814d22eb1b0e25b265",
+      comments : [ ]
     },
     {
       name: 'Cordillera',
       image:"/images/camp2.jpg",
       description: "A cordillera is an extensive chain of mountains or mountain ranges. The term is a borrowing from Spanish, in which it has the same meaning. The Spanish word originates from cordilla, a diminutive of <em>cuerda</em>, or <em>rope</em>. It is most commonly used in the field of physical geography.",
-      comments: []
+      user: "novak",
+      userid : "5cc966814d22eb1b0e25b265",
+      comments : [ ]
     },
     {
       name:'Caraterra Austral',
       image:"/images/camp3.jpg",
-      description:"The Carretera Austral (CH-7, in english: Southern Way) is the name given to Chile's Route 7. The highway runs about 1,240 kilometers (770 mi) from Puerto Montt to Villa O'Higgins through rural Patagonia.[1] Carretera Austral provides road access to Chile's Aysén del General Carlos Ibáñez del Campo Region and southern part of Los Lagos Region. These areas are sparsely populated and despite its length, Carretera Austral provides access to only about 100,000 people. South of the highway's start in Puerto Montt, Coyhaique (population 44,850[2]) is the largest city along it."
+      description:"The Carretera Austral (CH-7, in english: Southern Way) is the name given to Chile's Route 7. The highway runs about 1,240 kilometers (770 mi) from Puerto Montt to Villa O'Higgins through rural Patagonia.[1] Carretera Austral provides road access to Chile's Aysén del General Carlos Ibáñez del Campo Region and southern part of Los Lagos Region. These areas are sparsely populated and despite its length, Carretera Austral provides access to only about 100,000 people. South of the highway's start in Puerto Montt, Coyhaique (population 44,850[2]) is the largest city along it.",
+      user: "roger",
+      userid : "",
+      comments : [ ]
     }
   ];
 
