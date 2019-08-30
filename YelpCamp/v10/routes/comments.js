4673f1de58c356c9b4f1a7f30473e9601eb3ff61
ee7klt <kltah1@gmail.com>
2019-08-30 20:48:23 +0800

    checkCommentOwnership

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -20,7 +20,7 @@ router.get('/new', isLoggedIn, function(req,res) {
 
 
  // FORM edit comment
- router.get('/:comment_id/edit',  function(req,res) {
+ router.get('/:comment_id/edit',checkCommentOwnership,  function(req,res) {
    var camp_id = req.params.id;
    var comment_id = req.params.comment_id;
    camp.findById(camp_id, (err, campground) => {
@@ -29,7 +29,7 @@ router.get('/new', isLoggedIn, function(req,res) {
      } else {
       // res.send('comment edit')
       var comment = campground.comments.id(comment_id);
-      console.log(comment)
+      console.log('authorized. rendering edit page')
       //res.send('edit comment')
       res.render("comments/edit", {camp_id: camp_id, comment_id:comment_id, comment: comment})
      }
@@ -40,7 +40,7 @@ router.get('/new', isLoggedIn, function(req,res) {
 
 
  // UPDATE comment
- router.put('/:comment_id',function(req,res) {
+ router.put('/:comment_id',checkCommentOwnership,function(req,res) {
    var camp_id = req.params.id;
    var comment_id = req.params.comment_id;
    camp.findById(camp_id, (err,campground) => {
@@ -81,15 +81,15 @@ router.post('/', isLoggedIn, function(req, res) {
 })
 
 // DELETE comment
-router.delete('/:commentid/',function(req,res) {
+router.delete('/:comment_id/',checkCommentOwnership,function(req,res) {
   var id = req.params.id;
-  var commentid = req.params.commentid;
+  var comment_id = req.params.comment_id;
   camp.findById(id, (err, campground) => {
     if (err) {
       console.log('unable to retrieve camground for delete')
     } else {
-      var commentToBeRemoved = campground.comments.id(commentid).comment;
-      campground.comments.id(commentid).remove();
+      var commentToBeRemoved = campground.comments.id(comment_id).comment;
+      campground.comments.id(comment_id).remove();
       campground.save((err) => {
         if (err) console.log('error removing comment');
         console.log('removed comment "'+commentToBeRemoved+'"');
@@ -116,3 +116,41 @@ function isLoggedIn(req,res,next) {
 }
 
 module.exports=router;
+
+
+
+// middleware authorization (is logged in person authorized to edit/delete comment.)
+function checkCommentOwnership(req,res,next) {
+    var id = req.params.id;
+    var comment_id = req.params.comment_id;
+  // check for login first
+  if(req.isAuthenticated()) {
+    camp.findById(id, (err, campground) => {
+      if (err) {
+        console.log('unable to retrieve camground for comment authorization')
+      } else {
+        console.log('retrieved campground for comment authorization')
+        console.log(comment_id)
+          // if they own it, next()
+        if (req.user._id.equals(campground.comments.id(comment_id).userid)) {
+
+          console.log('authorized to edit/delete comment')
+          next()
+        } else {
+            // if they don't own it, redirect back.
+            // console.log('id of comment', comment_id)
+            // console.log('user id of comment:',campground.comments.id(comment_id).userid)
+            // console.log('user id of logged in user:',req.user._id )
+            console.log('user not authorized to edit comment')
+          res.redirect('back')
+        }
+
+      }
+    })
+  } else {
+    console.log("you need to be logged in to do that");
+      res.redirect('back')
+  }
+
+
+}
